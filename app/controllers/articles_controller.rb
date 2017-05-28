a1455e8548685a7c707cd3316bfa78770d171ab4
testapp <testapp@gmail.com>
2017-05-28 17:11:35 +0700

    index and edit controller adding

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,14 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles=Article.all
+  end
   def new
     @article=Article.new
+  end
+
+  def edit
+    @article=Article.find(params[:id])
   end
 
   def create
@@ -12,6 +20,17 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+
+  def update
+    @article=Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article updated succcessfully"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @article=Article.find(params[:id])
   end
@@ -21,6 +40,5 @@ class ArticlesController < ApplicationController
   private
     def article_params
       params.require(:article).permit(:title,:description)
-
     end
 end
