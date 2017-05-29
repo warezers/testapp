de71643bfd7ddbc4961f59a16dcb282cf9816620
testapp <testapp@gmail.com>
2017-05-29 13:47:26 +0700

    add boostrap files

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
   def index
     @articles=Article.all
   end
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article=Article.find(params[:id])
+
   end
 
   def create
@@ -22,7 +22,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article=Article.find(params[:id])
+
     if @article.update(article_params)
       flash[:notice] = "Article updated succcessfully"
       redirect_to article_path(@article)
@@ -32,12 +32,20 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article=Article.find(params[:id])
+
   end
 
+  def destroy
 
+    @article.destroy
+    flash[:notice] = "succcessfully deleted article"
+    redirect_to articles_path
+  end
 
   private
+    def set_article
+      @article=Article.find(params[:id])
+    end
     def article_params
       params.require(:article).permit(:title,:description)
     end
