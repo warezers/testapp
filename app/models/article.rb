6cb565a1ffbe40a024ba86ae6b0411f83df437e1
testapp <testapp@gmail.com>
2017-05-30 20:06:01 +0700

    index page design

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-  validates :title, presence: true,length: {minimum: 3, maximum: 50}
-  validates :description, presence: true,length: {minimum: 3, maximum: 50}
+  validates :title, presence: true,length: {minimum: 3, maximum: 400}
+  validates :description, presence: true,length: {minimum: 3, maximum: 400}
 end
