e8a40a4a54a3ed3a504ee612d1b643424ddb9dde
testapp <testapp@gmail.com>
2017-06-01 21:07:47 +0700

    user controller and form design

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
 
   resources :articles
 
+  get 'signup', to: 'users#new'
+  resources :users, except: '[:new]'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
